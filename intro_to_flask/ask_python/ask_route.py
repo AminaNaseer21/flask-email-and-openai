770ffab4e7b0557c4d86261da522ef0c25ae585d
Umer Seliya <hme36@txstate.edu>
2024-03-18 18:21:06 -0500

    Added the ref

--- intro_to_flask/ask_python/ask_route.py
+++ intro_to_flask/ask_python/ask_route.py
@@ -25,8 +25,43 @@ def askme():
         client = OpenAI()
         response = client.chat.completions.create(
           model="gpt-3.5-turbo",
+
+  "id": "modr-XXXXX",
+  "model": "text-moderation-005",
+  "results": [
+    {
+      "flagged": true,
+      "categories": {
+        "sexual": false,
+        "hate": false,
+        "harassment": false,
+        "self-harm": false,
+        "sexual/minors": false,
+        "hate/threatening": false,
+        "violence/graphic": false,
+        "self-harm/intent": false,
+        "self-harm/instructions": false,
+        "harassment/threatening": true,
+        "violence": true,
+      },
+      "category_scores": {
+        "sexual": 1.2282071e-06,
+        "hate": 0.010696256,
+        "harassment": 0.29842457,
+        "self-harm": 1.5236925e-08,
+        "sexual/minors": 5.7246268e-08,
+        "hate/threatening": 0.0060676364,
+        "violence/graphic": 4.435014e-06,
+        "self-harm/intent": 8.098441e-10,
+        "self-harm/instructions": 2.8498655e-11,
+        "harassment/threatening": 0.63055265,
+        "violence": 0.99011886,
+      }
+    }
+  ],
           messages=[
             #{"role": "system", "content": "You use Gen Z slang but are embarrassed when you do use it."},
+            
             {"role": "user", "content": form.prompt.data}
           ],
           max_tokens=150
